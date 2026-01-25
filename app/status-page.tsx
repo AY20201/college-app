1311265d7f6db22381cdd5a987c19504429fc91b
Alex Young <alexmy2018@gmail.com>
2026-01-24 22:05:48 -0500

    Fixed info panel

--- app/status-page.tsx
+++ app/status-page.tsx
@@ -172,7 +172,7 @@ export default function StatusPage() {
             getAppOpened().then((appOpened) => { 
                 console.log(appOpened);
                 setInfoPanelDisplayed(appOpened !== undefined ? !appOpened : true);
-                setAppOpened(false);
+                setAppOpened(true);
             }); //app opened (true) means don't display the panel
         }
         setPageLoaded(true);
