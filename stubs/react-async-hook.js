ec08b804b88eeb22b9d44eb53f6cf230da6648a1
Alex Young <alexmy2018@gmail.com>
2026-05-11 22:22:05 -0400

    Updated public path

--- stubs/react-async-hook.js
+++
@@ -1,3 +0,0 @@
-export const useAsync = () => ({ loading: false, result: undefined, error: undefined });
-export const useAsyncCallback = (fn) => [fn, { loading: false, result: undefined, error: undefined }];
-export default {};
