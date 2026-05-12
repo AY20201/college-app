97c950d590b9bba3b443a744445ed7a0de344286
Alex Young <alexmy2018@gmail.com>
2026-05-11 21:56:38 -0400

    Updated react-async-hook package

--- metro.config.js
+++ metro.config.js
@@ -6,9 +6,9 @@ const path = require('path');
 
 // Disable strict package exports to allow resolution of internal files
 config.resolver.unstable_enablePackageExports = false;
-config.resolver.extraNodeModules = {
-    ...config.resolver.extraNodeModules,
-    'react-async-hook': path.resolve(__dirname, 'stubs/react-async-hook.js'),
-};
+// config.resolver.extraNodeModules = {
+//     ...config.resolver.extraNodeModules,
+//     'react-async-hook': path.resolve(__dirname, 'stubs/react-async-hook.js'),
+// };
 
 module.exports = config;
