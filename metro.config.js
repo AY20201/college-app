791635edc6f34e0b0de7ae7cda0c40bed661c322
Alex Young <alexmy2018@gmail.com>
2026-05-11 21:40:52 -0400

    Fixed config file issue

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,7 @@ const { getDefaultConfig } = require('@expo/metro-config');
 
 /** @type {import('@expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+const path = require('path');
 
 // Disable strict package exports to allow resolution of internal files
 config.resolver.unstable_enablePackageExports = false;
